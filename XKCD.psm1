edaba4bd6b625cebb9d22463293c99200c8c3108
Mark Wragg <mark.wragg@gmail.com>
2017-01-17 21:44:37 +0000

    First commit
    Initial commit of my Get-XKCD function which I was previously developing in Gist.

---
+++ XKCD.psm1
@@ -0,0 +1,26 @@
+Function Get-XKCD{
+    [cmdletbinding(DefaultParameterSetName=’Specific’)]
+    Param (
+        [Parameter(ParameterSetName=’Random’)][switch]$Random,
+        [Parameter(ParameterSetName=’Random’)][int]$Min = 1,
+        [Parameter(ParameterSetName=’Random’)][int]$Max = (Invoke-RestMethod "http://xkcd.com/info.0.json").num,        
+        [Parameter(ParameterSetName=’Newest’)][int]$Newest,
+        [switch]$Download,
+        [ValidateScript({Test-Path $_ -PathType ‘Container’})] 
+        [string]$Path = $PWD,
+        [Parameter(ParameterSetName=’Specific’,ValueFromPipeline=$True,Position=0)][int[]]$Num = $Max
+       
+    )
+    Begin{
+        If ($Random) { $Num = get-random -min $Min -max $Max }
+        If ($Newest) { $Num = (($Max - $Newest)+1)..$Max }
+    }
+    Process{
+        $Num | ForEach-Object { 
+            $Comic = Invoke-RestMethod "http://xkcd.com/$_/info.0.json"
+            $Comic
+
+            If ($Download) { Invoke-WebRequest $Comic.img -OutFile $(Join-Path $Path "$_.jpg") }
+        }
+    }
+}
