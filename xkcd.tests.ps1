842e62b5485451139aca8f3faae34cd83ef3bc09
Mark Wragg <mark.wragg@gmail.com>
2017-01-19 16:45:07 +0000

    Adding appveyor config and pester tests
    Just to experiment with appveyor.

---
+++ xkcd.tests.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module XKCD
+
+Describe 'Basic Tests' {
+    $Latest = Get-XKCD
+
+    It 'Get-XKCD should return a PSCustomObject' {
+        $Latest | Should BeOfType 'System.Management.Automation.PSCustomObject'
+    }
+
+    It 'Get-XKCD should return an integer' {
+        $Latest.num | Should BeOfType [int]
+    }
+
+    #Not a good test but just for fun
+    It "Get-XKCD should return a comic from $((get-date).Year)" {
+        ($Latest).year | Should Be (get-date).Year
+    }
+
+
+}
