bbf8ff5dd0e210a6bb8fd06b0585af806e2f48c6
Mark Wragg <mark.wragg@gmail.com>
2017-01-19 23:45:07 +0000

    Update XKCD.tests.ps1

--- XKCD.tests.ps1
+++ XKCD.tests.ps1
@@ -1,18 +1,22 @@
 Import-Module $PSScriptRoot\XKCD.psm1 -Force
 
 Describe 'Basic Tests' {
-    $Latest = Get-XKCD
 
-    It 'Get-XKCD should return a PSCustomObject' {
-        $Latest | Should BeOfType 'System.Management.Automation.PSCustomObject'
-    }
+    Context 'Latest comic tests' {
 
-    It 'Get-XKCD should return an integer' {
-        $Latest.num | Should BeOfType [int]
-    }
+        $Latest = Get-XKCD
 
-    #Not a good test but just for fun
-    It "Get-XKCD should return a comic from $((get-date).Year)" {
-        ($Latest).year | Should Be (get-date).Year
+        It 'Get-XKCD should return a PSCustomObject' {
+            $Latest | Should BeOfType 'System.Management.Automation.PSCustomObject'
+        }
+
+        It 'Get-XKCD should return an integer' {
+            $Latest.num | Should BeOfType [int]
+        }
+
+        #Not a good test but just for fun
+        It "Get-XKCD should return a comic from $((get-date).Year)" {
+            ($Latest).year | Should Be (get-date).Year
+        }
     }
 }
