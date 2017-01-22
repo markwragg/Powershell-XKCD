ec056536bca82a4696fb27e580b12255002b2aa8
Mark Wragg <mark.wragg@gmail.com>
2017-01-22 09:17:09 +0000

    Update XKCD.tests.ps1

--- Tests/XKCD.tests.ps1
+++ Tests/XKCD.tests.ps1
@@ -1,6 +1,13 @@
-Import-Module XKCD -Force
+$projectRoot = Resolve-Path "$PSScriptRoot\.."
+$moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psm1")
+$moduleName = Split-Path $moduleRoot -Leaf
 
 Describe 'Basic Tests' {
+
+    It "Module '$moduleName' can import cleanly" {
+        {Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force } | Should Not Throw
+    }
+
     $Latest = Get-XKCD
 
     Context 'Latest Comic Tests' {
