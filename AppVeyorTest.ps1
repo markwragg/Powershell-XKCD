fb196e273d995211d6c48d9f4e078f8bfefa18a6
Mark Wragg <mark.wragg@gmail.com>
2017-01-22 08:53:29 +0000

    Update AppVeyorTest.ps1

--- AppVeyorTest.ps1
+++ AppVeyorTest.ps1
@@ -8,7 +8,7 @@ Write-Host "Current working directory: $pwd"
 # Run Pester Tests                # 
 #---------------------------------# 
 $testResultsFile = '.\TestsResults.xml'
-$res             = Invoke-Pester -Script .\Tests\Default.Tests.ps1 -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
+$res             = Invoke-Pester -Script .\Tests\*.Tests.ps1 -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
 
 Write-Host 'Uploading results'
 (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
