dd4c1466c21ce7269fb7f39c89e80f7149392160
Mark Wragg <mark.wragg@gmail.com>
2017-01-23 09:36:34 +0000

    Update AppVeyorTest.ps1

--- AppVeyorTest.ps1
+++ AppVeyorTest.ps1
@@ -2,21 +2,23 @@ Write-Host 'Running AppVeyor test script' -ForegroundColor Yellow
 Write-Host "Current working directory: $pwd"
 
 # Run Pester Tests
-$testResultsFile = '.\TestsResults.xml'
-$res             = Invoke-Pester -Script .\Tests\*.Tests.ps1 -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
+$testResultsFile = 'TestsResults.xml'
+$Results = Invoke-Pester -Script .\Tests\*.Tests.ps1 -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
 
-#Workaround for https://github.com/appveyor/ci/issues/1271
-[xml]$content = Get-Content $testResultsFile
-$content.'test-results'.'test-suite'.type = "Powershell"
-$content.Save('C:\projects\powershell-xkcd\TestsResults.xml')
-#Remove workaround when issue fixed
+If ($env:APPVEYOR){
+    #Workaround for https://github.com/appveyor/ci/issues/1271
+    [xml]$Content = Get-Content $testResultsFile
+    $Content.'test-results'.'test-suite'.type = "Powershell"
+    $Content.Save("$(Join-Path $pwd $testResultsFile)")
+    #Remove workaround when issue 1271 fixed
 
-Write-Host 'Uploading results'
-(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
+    Write-Host 'Uploading results'
+    (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
+}
 
 # Validate
-If (($res.FailedCount -gt 0) -or ($res.PassedCount -eq 0)) { 
-    throw "$($res.FailedCount) tests failed."
-} else {
-  Write-Host 'All tests passed' -ForegroundColor Green
+If (($Results.FailedCount -gt 0) -or ($Results.PassedCount -eq 0)) { 
+    throw "$($Results.FailedCount) tests failed."
+} Else {
+    Write-Host 'All tests passed' -ForegroundColor Green
 }
