22811ad27011c7d884d3682338000b7bff007560
Mark Wragg <mark.wragg@gmail.com>
2017-01-23 10:40:28 +0000

    Update AppVeyorDeploy.ps1

--- AppVeyorDeploy.ps1
+++ AppVeyorDeploy.ps1
@@ -10,7 +10,7 @@ Function Get-ModuleContents {
     Get-ChildItem -Exclude *.psd1 $Path | Where-Object { -not $_.PsIsContainer } | Get-Content
 }
 
-# Check module for differences 
+Write-Host 'Checking module for differences with existing PS Gallery version'
 Save-Module -Name $env:ModuleName -Path .\
 
 $GitModuleContents = Get-ModuleContents .\$env:ModuleName\
@@ -19,13 +19,11 @@ $PSGModuleContents = Get-ModuleContents .\$env:ModuleName\*\*
 $Differences = Compare-Object -ReferenceObject $GitModuleContents -DifferenceObject $PSGModuleContents
 
 If ($Differences){
-    # Update module manifest
     Write-Host 'Creating new module manifest'
     $ModuleManifestPath = Join-Path -path "$pwd\$env:ModuleName" -ChildPath ("$env:ModuleName"+'.psd1')
     $ModuleManifest     = Get-Content $ModuleManifestPath -Raw
     [regex]::replace($ModuleManifest,'(ModuleVersion = )(.*)',"`$1'$env:APPVEYOR_BUILD_VERSION'") | Out-File -LiteralPath $ModuleManifestPath
 
-    # Publish to PS Gallery
     $ModulePath = Split-Path "$pwd\$env:ModuleName"
     Write-Host "Adding $ModulePath to 'psmodulepath' PATH variable"
     $env:psmodulepath = $env:psmodulepath + ';' + $ModulePath
