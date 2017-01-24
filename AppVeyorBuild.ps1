be84c5629972712655379029983fee2877b1eaa5
Mark Wragg <mark.wragg@gmail.com>
2017-01-24 11:31:37 +0000

    Merged the four appveyor scripts in to one
    I think it's cleaner from a repo perspective to keep all the appveyor
    code in one .ps1 file so i've merged the 4 scripts together and have
    them called via params.

--- AppVeyorBuild.ps1
+++
@@ -1,5 +0,0 @@
-﻿Write-Host 'Running AppVeyor build script' -ForegroundColor Yellow
-Write-Host "ModuleName    : $env:ModuleName"
-Write-Host "Build version : $env:APPVEYOR_BUILD_VERSION"
-Write-Host "Author        : $env:APPVEYOR_REPO_COMMIT_AUTHOR"
-Write-Host "Branch        : $env:APPVEYOR_REPO_BRANCH"
