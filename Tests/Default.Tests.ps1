16ee7c92dc58e9ce63da04f47bab546d60e9292e
Mark Wragg <MWragg@omnicogroup.com>
2019-03-21 13:27:04 +0000

    Excluded whitespace rule

--- Tests/Default.Tests.ps1
+++ Tests/Default.Tests.ps1
@@ -1,7 +1,9 @@
 ﻿# PSScriptAnalyzer tests
 $Scripts = Get-ChildItem “$PSScriptRoot/../” -Filter ‘*.ps1’ -Recurse | Where-Object { $_.name -NotMatch 'tests.ps1' -and $_.name -NotIn 'build.ps1','psake.ps1' }
 $Modules = Get-ChildItem “$PSScriptRoot/../” -Filter ‘*.psm1’ -Recurse
-$Rules   = Get-ScriptAnalyzerRule
+
+$ExcludeRules = 'PSAvoidTrailingWhitespace','PSAvoidGlobalVars'
+$Rules   = Get-ScriptAnalyzerRule | Where-Object { $_.RuleName -notin $ExcludeRules }
 
 If ($Modules.count -gt 0) {
   Describe ‘Testing all Modules against default PSScriptAnalyzer rule-set’ {
